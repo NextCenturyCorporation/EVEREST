6bb9823a24cbccfccae8aa620f677b2cef856c89
Josh Williams <halfs13@gmail.com>
2013-06-18 11:30:30 -0400

    Fixing root url

--- services/router_service.js
+++ services/router_service.js
@@ -3,17 +3,12 @@ this.load = function(app, io, gcm, logger){
 	require('./rest/alpha_report.js').load(app, io, gcm, logger);
 	require('./rest/assertion.js').load(app, io, gcm, logger);
 	require('./rest/confirmed_report.js').load(app, io, gcm, logger);
+	require('./rest/event.js').load(app, io, gcm, logger);
 	require('./rest/incident.js').load(app, io, gcm, logger);
 	require('./rest/comment.js').load(app, io, gcm, logger);
 	require('./rest/options.js').load(app, io, gcm, logger);
 	
-	var event = require('./rest/event.js');
-	event.load(app, io, gcm, logger);
-	
 	app.get('/', function(req, res){
-		if(logger.DO_LOG){
-			logger.info('Request for list of events');
-		}
-		event.listEvents(req.query, res);
+		res.redirect('/event');
 	});
 };
