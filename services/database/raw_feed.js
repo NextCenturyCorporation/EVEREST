057df5c68087641e786df11fdcc8939268b6c760
Evan Nehring <evan.d.nehring@gmail.com>
2013-06-19 13:39:37 -0400

    Add code to satisfy CRUD services. Uses database/location.js as a template

--- services/database/raw_feed.js
+++ services/database/raw_feed.js
@@ -13,21 +13,84 @@ var logger = new (winston.Logger)({
 });
 
 this.listFeeds = function(opts, res){
-
+	models.rawFeed.find({},'_id name', function(err, docs){
+		if(err){
+			logger.info("Error listing raw feeds "+err);
+			res.status(500);
+			res.json({error: 'Error'});
+		} else {
+			res.json(docs);
+		}
+		res.end();
+	});
 };
 
 this.createFeed = function(data, res, io, gcm){
-
+	var newFeed = new models.rawFeed(data);
+	newFeed.save(function(err){
+		if(err){
+			logger.error('Error saving raw feed', err);
+			general.send500(res);
+		} else {
+			res.json({id:newFeed._id});
+			res.end();
+		}
+	});
 };
 
 this.getFeed = function(index, opts, res){
-
+	models.rawFeed.findById(id, function(err, docs){
+		if(err) {
+			logger.info("Error getting raw feed "+err);
+			res.status(500);
+			res.json({error: 'Error'});
+		} else if(docs) {
+			res.json(docs);
+		} else {
+			res.status(404);
+			res.json({error: 'Not found'});
+		}
+		res.end();
+	});
 };
 
 this.updateFeed = function(index, data, res){
-
+	models.rawFeed.findById(id, function(err, docs){
+		if(err) {
+			logger.info("Error getting raw feed "+err);
+			general.send500(res);
+		} else if(docs) {
+			for(var e in data){
+				//Make sure not to change _id
+				if(e != '_id'){
+					docs[e] = data[e];
+				}
+			}
+			docs.save(function(err){
+				if(err){
+					general.send500(res);
+				} else {
+					res.end({status:'ok'});
+					res.end();
+				}
+			});
+		} else {
+			general.send404(res);
+		}
+	});
 };
 
 this.deleteFeed = function(index, res){
-
+	models.rawFeed.find({_id:id}, function(err, docs){
+		if(err || docs.length === 0){
+			logger.error('Error deleting raw feed', err);
+			general.send500(res);
+		} else {
+			for(var i = 0; i < docs.length; i++){
+				docs[i].remove();
+			}
+			res.json({status:'ok'});
+			res.end();
+		}//;
+	});
 };
