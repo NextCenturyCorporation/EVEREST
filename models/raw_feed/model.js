8fa40a5e81a97ba5a14d9dcc20826fd46da02cfd
Evan Nehring <evan.d.nehring@gmail.com>
2013-06-27 10:51:32 -0400

    Add validation to the raw feed model

--- models/raw_feed/model.js
+++ models/raw_feed/model.js
@@ -13,3 +13,24 @@ this.rawFeedModel = {
 
 var rawFeedSchema = new Schema(this.rawFeedModel);
 this.rawFeed = mongoose.model('rawFeed', rawFeedSchema);
+
+rawFeedValidation = {
+	properties: {
+		timestamp: {
+			description: 'Time this raw feed was created in datastore',
+			type: 'date'
+		},
+		text: {
+			description: 'The contents of the raw feed',
+			type: 'string'
+		},
+		feedSource: {
+			description: 'The source of the raw feed',
+			type: 'string',
+			enum: ['Twitter', 'Email'],
+			messages: {
+				enum: 'Expected a source of Twitter or Email'
+			}
+		}
+	}
+};
