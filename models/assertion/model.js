05065f605654b4b81dfa92524f0eeae3f770b919
Evan Nehring <evan.d.nehring@gmail.com>
2013-07-11 15:57:24 -0400

    Define assertion model and validation

--- models/assertion/model.js
+++ models/assertion/model.js
@@ -7,8 +7,50 @@ var ObjectId = Schema.ObjectId;
 //var config = require('../../config.js');
 
 this.assertionModel = {
-		timestamp: {type: Date, "default": Date.now},
-		alpha_report_id: ObjectId
+	createdDate: {type: Date},
+	updatedDate: {type: Date},	
+	alpha_report_id: {type: String, required: true},
+	reporter_id: {type: String},
+	entity1: {type: String, required: true},
+	relationship: {type: String, required: true},
+	entity2: {type: String, required: true}
 };
 var AssertionSchema = new Schema(this.assertionModel);
 this.assertion = mongoose.model('Assertion', AssertionSchema);
+
+this.assertionValidation= {
+	properties: {
+		createdDate: {
+			description: 'Date this assertion was created in datastore',
+			type: 'date'
+		},
+		updatedDate: {
+			description: 'Date this assertion was last updated in datastore',
+			type: 'date'
+		},
+		alpha_report_id: {
+			description: 'ID of the corresponding alpha report',
+			type: 'string',
+			required: true
+		},
+		reporter_id: {
+			description: 'ID of the reporter who submitted the alpha report',
+			type: 'string',
+		},
+		entity1: {
+			description: 'The subject of the raw feed',
+			type: 'string',
+			required: true
+		},
+		relationship: {
+			description: 'The verb connecting the two entities',
+			type: 'string',
+			required: true
+		},
+		entity2: {
+			description: 'The object of the raw feed',
+			type: 'string',
+			required: true
+		}
+	}	
+};
