a797d0ee350866fde7dae26f5e3481f79a735015
bnewsom <benjamin.newsom@gmail.com>
2013-07-30 10:25:56 -0400

    moved semantic and business validation down to the data access layer

--- services/database/location.js
+++ services/database/location.js
@@ -1,9 +1,11 @@
 /**
  * Runs while connected to a database
  */
-
 var winston = require('winston');
 var models = require('../../models/models');
+var validationModel = require('../../models/location/model.js');
+var bvalidator = require('../../models/location/bvalidator.js');
+var revalidator = require('revalidator');
 
 //Load and set up the logger
 var logger = new (winston.Logger)({
@@ -53,19 +55,75 @@ this.listLocationNames = function(res){
  * On success, it returns id:<ID-hash>
  */
 this.createLocation = function(data, res){
-	var newLoc = new models.location(data);
-	newLoc.createdDate = new Date();
-	newLoc.updatedDate = new Date();
-	newLoc.save(function(err){
+	this.saveLocation(data, function(err, val, newLoc) {
 		if(err){
 			logger.error('Error saving location', err);
 			res.status(500);
 			res.json({error: 'Error'});
+		} else if (!val.valid) {
+			logger.info('Invalid location ' + JSON.stringify(val.errors));
+			res.status(500);
+			res.json({error: val.errors}, data);
 		} else {
+			logger.info('Location saved ' + JSON.stringify(newLoc));
 			res.json({id:newLoc._id});
-			res.end();
+		}
+		res.end();
+	});
+};
+
+/**
+ * saveLocation is a "generic" save method callable from both
+ * request-response methods and parser-type methods for population of location data
+ * 
+ * saveLocation calls the validateLocation module to ensure that the
+ * location data being saved to the database is complete and has integrity.
+ * 
+ * saveCallback takes the form of  function(err, valid object, location object)
+ */
+this.saveLocation = function(data, saveCallback) {
+	this.validateLocation(data, function(valid) {
+		if (valid.valid) {
+			logger.info("Valid location");
+			var newLoc = new models.location(data);
+			newLoc.createdDate = new Date();
+			newLoc.updatedDate = new Date();
+			newLoc.save(function(err){
+				if(err){
+					logger.error('Error saving location', err);
+				}
+				saveCallback(err, valid, newLoc);
+			});
+		}
+		else {
+			saveCallback(undefined, valid, data);
 		}
 	});
+};
+
+/**
+ * validateLocation validates a location object against the location semantic rules
+ * and the business rules associated with a location
+ *
+ * saveLocation calls the JSON validation module  revalidator and
+ * calls the business validation module bvalidator for the location object
+
+ * data is the location object being validated
+ * 
+ * valCallback takes the form of  function(valid structure)
+ */
+this.validateLocation = function(data, valCallback) {
+	// is the JSON semantically valid for the location object?
+	var valid = revalidator.validate(data, validationModel.locationValidation);
+	if (valid.valid) {
+		// does the location object comply with business validation logic
+		bvalidator.validate(data, function(valid) {
+			valCallback(valid);
+		});
+	}
+	else {
+		valCallback(valid);
+	}	
 };
 
 /**
@@ -104,22 +162,26 @@ this.getLocationByName = function(value, res){
 	});
 };
 
-
+/**
+ * readLocationByProperty is a generic read method to return all of
+ * documents that have a property value that matches.
+ * 
+ * The readCallback should be function(err, docs)
+ */
 this.readLocationByProperty = function(property, value, readCallback){
 	var query = models.location.find({});
 	query.where(property, value);
 	query.exec(readCallback);
-//	query.exec(function(err, docs){
-//		if(err) {
-//			logger.info({error: "Error getting locationByName "+err});
-//		} else if(0 !== docs.length) {
-//			logger.info("Location found " + JSON.stringify(docs));
-//		} else {
-//			logger.info({error: "Not found"});
-//		}
-//	});
 };
 
+/**
+ * readAssertionByObject is a generic read method for assertion
+ * It will attempt to find an exact match(es) for the object provided.
+ * 
+ * Note: the incoming object can be a subset or superset of the actual object.
+ * 
+ * The readCallback should be function(err, docs)
+ */
 this.readLocationByObject = function(object, readCallback){
 	var query = models.location.find({});
 	for (var key in object) {
@@ -152,31 +214,59 @@ this.searchLocation = function(data, res){
  * On success, it returns the _id value (just like on create)
  */
 this.updateLocation = function(id, data, res){
-	models.location.findById(id, function(err, docs){
-		if(err) {
-			logger.info("Error getting location "+err);
+	this.updateLocationX(id, data, function(err, val, updLoc) {
+		if(err){
+			logger.error('Error updating location', err);
 			res.status(500);
 			res.json({error: 'Error'});
-		} else if(docs) {
-			for(var e in data){
-				//Make sure not to change _id
-				if(e !== '_id'){
-					docs[e] = data[e];
-				}
-			}
-			docs.updatedDate = new Date();
-			docs.save(function(err){
-				if(err){
-					res.status(500);
-					res.json({error: 'Error'});
-				} else {
-					res.json({id:docs._id});
-				}
-			});			
+		} else if (!val.valid) {
+			logger.info('Invalid location ' + JSON.stringify(val.errors));
+			res.status(500);
+			res.json({error: val.errors}, data);
 		} else {
-			res.status(404);
+			logger.info('Location updated ' + JSON.stringify(updLoc));
+			res.json({id:updLoc._id});
 		}
 		res.end();
+	});
+};
+/**
+ *
+ * callback takes the form of  function(err, valid object, location object)
+ * 
+ */
+this.updateLocationX = function(id, data, updCallback) {
+	this.validateLocation(data, function(valid){
+		if (valid.valid) {
+			models.location.findById(id, function(err, docs){
+				if(err) {
+					logger.info("Error getting location "+err);
+					updCallback(err, valid, data);
+				} else if(docs) {
+					for(var e in data){
+						//Make sure not to change _id
+						if(e !== '_id'){
+							docs[e] = data[e];
+						}
+					}
+					docs.updatedDate = new Date();
+					docs.save(function(err){
+						if(err){
+							updCallback(err, valid, data);
+						} else {
+							updCallback(err, valid, docs);
+						}
+					});			
+				} else {
+					valid.valid = false;
+					valid.errors = {expected: id, message: "Location not found"};
+					updCallback(err, valid, data);
+				}
+			});
+		}
+		else {
+			updCallback(undefined, valid, data);
+		}
 	});
 };
 
--- services/rest/location.js
+++ services/rest/location.js
@@ -1,7 +1,4 @@
 var locationService = require('../database/location.js');
-var validationModel = require('../../models/location/model.js');
-var bvalidator = require('../../models/location/bvalidator.js');
-var revalidator = require('revalidator');
 
 this.load = function(app, io, gcm, logger) {
 	//list - lists full object
@@ -25,33 +22,9 @@ this.load = function(app, io, gcm, logger) {
 		if(logger.DO_LOG){
 			logger.info("Receiving new location");
 		}
-		// is the JSON semantically valid for the location object?
-		var locVal = revalidator.validate(req.body, validationModel.locationValidation);
-		if (locVal.valid) {
-			// does the location object comply with business validation logic
-			bvalidator.validate(req.body, function(bVal) {
-				if (bVal.valid) {
-					logger.info("Valid location");
-					locationService.createLocation(req.body, res);				
-				}
-				else {
-					if(logger.DO_LOG){
-						logger.error(bVal.errors);
-					}
-					res.status(500);
-					res.json({error: bVal.errors}, req.body);
-				}
-			});
-		}
-		else {
-			if(logger.DO_LOG){
-				logger.error(locVal.errors);
-			}
-			res.status(500);
-			res.json({error: locVal.errors}, req.body);
-		}
+		locationService.createLocation(req.body, res);				
 	});
-	
+
 	//review
 	app.get('/location/:id([0-9a-f]+)', function(req,res){
 		if(logger.DO_LOG){
@@ -80,17 +53,7 @@ this.load = function(app, io, gcm, logger) {
 		if(logger.DO_LOG){
 			logger.info("Update location "+req.params.id);
 		}
-		var locVal = revalidator.validate(req.body, validationModel.locationValidation);
-		if (locVal.valid) {
-			locationService.updateLocation(req.params.id, req.body, res);
-		}
-		else {
-			if(logger.DO_LOG){
-				logger.info(locVal.errors);
-			}
-			res.status(500);
-			res.json({error: locVal.errors});
-		}
+		locationService.updateLocation(req.params.id, req.body, res);
 	});
 	
 	//delete by id
