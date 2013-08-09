4b014fc0027211937d657de9a2e2bf85028677a5
Ashley <aviendha2281@gmail.com>
2013-08-09 10:09:42 -0400

    skip alpha reports with no message_body field

--- services/parsers/nlp_parser.js
+++ services/parsers/nlp_parser.js
@@ -18,37 +18,38 @@ this.parseAndSave = function(alpha_report_object, callback){
 	logger.info('Attempting to parse alpha_report_object with id ' + alpha_report_object._id);
 		
 	var assertion_object = {};
-	
-	var tree = parser.parseSync(alpha_report_object.message_body);
-	for (var i = 0; i < tree.sizeSync(); i++){
-		//get first sentence fragment from parsed message
-		var leaf = tree.getSync(i);
-		var numKids = leaf.lastChildSync().numChildrenSync();
-	
-		//get rid of punctuation?
-		var last = leaf.lastChildSync().lastChildSync().toStringSync();
-		if (last.indexOf("(. ") === 0){
-			leaf.lastChildSync().removeChildSync(numKids - 1);
-		}
-		var output = extractor.extractTripletSync(leaf);
+	if(alpha_report_object.message_body){
+		var tree = parser.parseSync(alpha_report_object.message_body);
+		for (var i = 0; i < tree.sizeSync(); i++){
+			//get first sentence fragment from parsed message
+			var leaf = tree.getSync(i);
+			var numKids = leaf.lastChildSync().numChildrenSync();
 		
-		//create an assertion object for each section of this message body
-		if (output){
-			assertion_object.alpha_report_id = alpha_report_object._id.toString();
-			assertion_object.reporter_id = alpha_report_object.reporter_id.toString();
-			assertion_object.entity1 = output.getEntity1StringSync().toString();
-			assertion_object.relationship = output.getRelationStringSync().toString();
-			assertion_object.entity2 = output.getEntity2StringSync().toString();
+			//get rid of punctuation?
+			var last = leaf.lastChildSync().lastChildSync().toStringSync();
+			if (last.indexOf("(. ") === 0){
+				leaf.lastChildSync().removeChildSync(numKids - 1);
+			}
+			var output = extractor.extractTripletSync(leaf);
 			
-			assertion_service.saveAssertion(assertion_object, function(err, valid, newAssertion){
-				if (err){
-					logger.info("There was an error saving the parsed Assertion object.");
-				} else if(!valid.valid){
-					logger.info("Invalid assertion " + JSON.stringify(valid.errors));
-				} else {
-					logger.info("Callback version " + newAssertion);
-				}
-			});
+			//create an assertion object for each section of this message body
+			if (output){
+				assertion_object.alpha_report_id = alpha_report_object._id.toString();
+				assertion_object.reporter_id = alpha_report_object.reporter_id.toString();
+				assertion_object.entity1 = output.getEntity1StringSync().toString();
+				assertion_object.relationship = output.getRelationStringSync().toString();
+				assertion_object.entity2 = output.getEntity2StringSync().toString();
+				
+				assertion_service.saveAssertion(assertion_object, function(err, valid, newAssertion){
+					if (err){
+						logger.info("There was an error saving the parsed Assertion object.");
+					} else if(!valid.valid){
+						logger.info("Invalid assertion " + JSON.stringify(valid.errors));
+					} else {
+						logger.info("Callback version " + newAssertion);
+					}
+				});
+			}
 		}
 	}
 };
