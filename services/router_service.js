194758a2d7fa9ec59875a78e2944c442cdac9882
Josh Williams <josh.williams@nextcentury.com>
2013-08-15 13:15:47 -0400

    Cleaning up jshint

--- services/router_service.js
+++ services/router_service.js
@@ -3,48 +3,48 @@
 
 var models = require('../models/models');
 
-var routerService = module.exports = function(app, io, logger){
+module.exports = function(app, io, logger){
 	var AlphaReport = require('./rest/alpha_report.js');
-	var alphaReport = new AlphaReport(app, models, io, logger);
+	new AlphaReport(app, models, io, logger);
 	
 	var Assertion = require('./rest/assertion.js');
-	var asserion = new Assertion(app, models, io, logger);
+	new Assertion(app, models, io, logger);
 
 	var Comment = require('./rest/comment.js');
-	var comment = new Comment(app, models, io, logger);
+	new Comment(app, models, io, logger);
 
 	var ConfirmedReport = require('./rest/confirmed_report.js');
-	var confirmedReport = new ConfirmedReport(app, models, io, logger);
+	new ConfirmedReport(app, models, io, logger);
 
 	var Event = require('./rest/event.js');
-	var event = new Event(app, models, io, logger);
+	new Event(app, models, io, logger);
 	
 	var Incident = require('./rest/incident.js');
-	var incident = new Incident(app, models, io, logger);
+	new Incident(app, models, io, logger);
 
 	var Location = require('./rest/location.js');
-	var location = new Location(app, models, io, logger);
+	new Location(app, models, io, logger);
 	
 	var NlpParser = require('./rest/nlp_parser_service.js');
-	var nlpParser = new NlpParser(app, models, io, logger);
+	new NlpParser(app, models, io, logger);
 	
 	var Options = require('./rest/options.js');
-	var options = new Options(app, models, io, logger);
+	new Options(app, models, io, logger);
 	
 	var Profile = require('./rest/profile.js');
-	var profile = new Profile(app, models, io, logger);
+	new Profile(app, models, io, logger);
 	
 	var RawFeed = require('./rest/raw_feed.js');
-	var rawFeed = new RawFeed(app, models, io, logger);
+	new RawFeed(app, models, io, logger);
 
 	var Reporter = require('./rest/reporter.js');
-	var reporter = new Reporter(app, models, io, logger);
+	new Reporter(app, models, io, logger);
 	
 	var TargetAssertion = require('./rest/target_assertion.js');
-	var targetAssertion = new TargetAssertion(app, models, io, logger);
+	new TargetAssertion(app, models, io, logger);
 	
 	var TwitterIngest = require('./rest/twitter_ingest_service.js');
-	var twitterIngest = new TwitterIngest(app, models, io, logger);
+	new TwitterIngest(app, models, io, logger);
 	
 	app.get('/', function(req, res){
 		res.redirect('/events');
