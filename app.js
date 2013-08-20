df98fd58606c375a0fb52e99729392d4e661a4ee
bnewsom <benjamin.newsom@gmail.com>
2013-08-20 16:21:56 -0400

    updated methods in Access-Control-Allow-Methods

--- app.js
+++ app.js
@@ -52,13 +52,13 @@ app.configure(function(){
 		}
 		// use "*" here to accept any origin
 		res.set('Access-Control-Allow-Origin', '*');
-		res.set('Access-Control-Allow-Methods', 'GET, POST', 'DEL', 'DELETE', 'PUT', 'SEARCH');
+		res.set('Access-Control-Allow-Methods', 'GET, POST, DEL, DELETE, PUT, SEARCH, OPTIONS');
 		res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
 		// res.set('Access-Control-Allow-Max-Age', 3600);
 		if ('OPTIONS' === req.method) {
 			return res.send(200);
 		}
-		next();
+		return next();
 	});
 
 	/**
