1472c6dbef5d762ebef81afbfd2d08e7671b3354
Josh Williams <josh.williams@nextcentury.com>
2013-08-23 12:18:18 -0400

    adding default api key

--- config.js
+++ config.js
@@ -9,7 +9,14 @@ config.db_port = process.env.DB_PORT || 27017;
 //Collection name
 config.db_collection = process.env.DB_COLLECTION || 'everest';
 
-config.log_level = process.env.LOG_LEVEL || 'info';
+config.log_level = process.env.LOG_LEVEL || 'debug';
+
+config.defaultTwitterKey = {
+	consumer_key: 'rwJPisTyYT2zzi0o4hpv2g',
+	consumer_secret: 'ncTw7IKtmCFwNyhQ8OOfshfGOOCPLPlDug0Em1oIg',
+	access_token_key: '53703582-dF6S7rtSzCBQtxbQnSkjy49ECSPMix7tXz2O8W0ej',
+	access_token_secret: 'G74GzFILLGmrjDV9cJSGQb6cRC4yk2sP5dD7lL8x2z0'
+}
 
 config.paramDefaults = {
 	listCount: null,
