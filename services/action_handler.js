701a0c185f343d3f7c714f36aafbe59cc6cde9ab
Micah Fulton <micah.fulton@nextcentuty.com>
2013-09-23 16:52:56 -0400

    Refactored the event modules so that logic for handler implementation is in its own file.

--- services/action_handler.js
+++ services/action_handler.js
@@ -52,56 +52,9 @@ module.exports = function(models, io, logger) {
 			serviceList[service.toObjectNotation()] = new requiredServicesList[service.toString()](models,io,logger);
 		}
 	}
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////ll
-	/**
-	*All you have to do, is for an event defined in action_emitter create a function with the same name followed by 
-	*"Handler" then you can call any of the methods defined in any of the services.
-	*THIS IS THE ONLY FUNCTION YOU NEED TO MODIFY TO IMPLEMENT NEW EVENT HANDLERS.
-	**/
-	var Listener = function(){
-		//This converts the arguments to an array so that any number of arguments can be passed to the method.
-		Function.prototype.callWithAllArgs = function() {
-			return this.apply(this, Array.prototype.slice.call(arguments[0][0], 0));
-		};
+	var Listener = require("./action_implementations.js");
+	var listener = new Listener(serviceList, io, logger);
 
-		this.saveAlphaReportEventHandler = function() {
-			io.sockets.to('EVEREST.data.workflow').emit('item_saved', {type: "AlphaReport"});
-			logger.debug("Emitted socket with item_saved for AlphaReport");
-		};
-
-		this.updateAlphaReportEventHandler = function() {
-			serviceList.AlphaReport.updateAlphaReportX.callWithAllArgs(arguments);
-		};
-
-		this.validateAlphaReportEventHandler = function() {
-			serviceList.AlphaReport.validateAlphaReport.callWithAllArgs(arguments);
-		};
-
-		this.rawFeedDataRecievedEventHandler = function() {
-			//Services that are prototyped out must be called this way.
-			var self = serviceList.RawFeed;
-			self.create.apply(self, Array.prototype.slice.call(arguments[0], 0));
-		};
-
-		this.saveFeedEventHandler = function() {
-			io.sockets.to('EVEREST.data.workflow').emit('item_saved', {type: "RawFeed"});
-			logger.debug("Emitted socket with item_saved for RawFeed");
-		};
-
-		this.rawFeedParseEventHandler = function() {
-			serviceList.RawFeedParser.parseAndSave.callWithAllArgs(arguments);
-		};
-
-		this.saveAssertionEventHandler = function() {
-			io.sockets.to('EVEREST.data.workflow').emit('item_saved', {type: "Assertion"});
-			logger.debug("Emitted socket with item_saved for Assertion");
-		};
-
-		//More Implemented event handlers below....
-	};
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	var listener = new Listener();
 	var eventerEventList = actionEmitter.eventList;
 
 	//Sets up the corresponding Handlers for each event defined in the action_emitter.js file.
---
+++ services/action_implementations.js
@@ -0,0 +1,46 @@
+/**
+*All you have to do, is for an event defined in action_emitter create a function with the same name followed by 
+*"Handler" then you can call any of the methods defined in any of the services.
+*THIS IS THE ONLY FUNCTION YOU NEED TO MODIFY TO IMPLEMENT NEW EVENT HANDLERS.
+**/
+module.exports =  Listener = function(serviceList, io ,logger){
+	//This converts the arguments to an array so that any number of arguments can be passed to the method.
+	Function.prototype.callWithAllArgs = function() {
+		return this.apply(this, Array.prototype.slice.call(arguments[0][0], 0));
+	};
+
+	this.saveAlphaReportEventHandler = function() {
+		io.sockets.to('EVEREST.data.workflow').emit('item_saved', {type: "AlphaReport"});
+		logger.debug("Emitted socket with item_saved for AlphaReport");
+	};
+
+	this.updateAlphaReportEventHandler = function() {
+		serviceList.AlphaReport.updateAlphaReportX.callWithAllArgs(arguments);
+	};
+
+	this.validateAlphaReportEventHandler = function() {
+		serviceList.AlphaReport.validateAlphaReport.callWithAllArgs(arguments);
+	};
+
+	this.rawFeedDataRecievedEventHandler = function() {
+		//Services that are prototyped out must be called this way.
+		var self = serviceList.RawFeed;
+		self.create.apply(self, Array.prototype.slice.call(arguments[0], 0));
+	};
+
+	this.saveFeedEventHandler = function() {
+		io.sockets.to('EVEREST.data.workflow').emit('item_saved', {type: "RawFeed"});
+		logger.debug("Emitted socket with item_saved for RawFeed");
+	};
+
+	this.rawFeedParseEventHandler = function() {
+		serviceList.RawFeedParser.parseAndSave.callWithAllArgs(arguments);
+	};
+
+	this.saveAssertionEventHandler = function() {
+		io.sockets.to('EVEREST.data.workflow').emit('item_saved', {type: "Assertion"});
+		logger.debug("Emitted socket with item_saved for Assertion");
+	};
+
+	//More Implemented event handlers below....
+};
