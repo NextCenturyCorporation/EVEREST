d45293cff5f4b69ef4f2edb1a7e2447f20471ce4
bnewsom <benjamin.newsom@gmail.com>
2013-10-08 12:56:17 -0400

    JSHint unused vars

--- models/reporter/model.js
+++ models/reporter/model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
+//var ObjectId = Schema.ObjectId;
 
 var reporterDataModel = {
 	createdDate: {type: Date},
