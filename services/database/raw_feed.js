1a1d085b6ca13d82ac8b8fcaa000ca5aeaf1e0d9
Ashley <aviendha2281@gmail.com>
2013-10-08 12:22:57 -0400

    retrying to change databases/raw_feed.js

--- services/database/raw_feed.js
+++ services/database/raw_feed.js
@@ -1,118 +1,119 @@
 var revalidator = require('revalidator');
 var actionEmitter = require('../action_emitter.js');
+var paramHandler = require('../list_default_handler');
 
-var RawFeed = module.exports = function(models, io, log) {
+module.exports = function(models, io, logger) {
 	var me = this;
-
-	me.logger = log;
-	me.models = models;
-	me.io = io;
-
-	me.validationModel = me.models.rawFeed.rawFeed;
-};
-
-RawFeed.prototype.list = function(opts, listCallback){
-	var me = this;
-
-	me.models.rawFeed.find({}, listCallback);
-};
-
-RawFeed.prototype.get = function(id, callback) {
-	var me = this;
+	var validationModel = models.rawFeedValidation;
 	
-	me.models.rawFeed.findById(id, callback);
-};
-
-RawFeed.prototype.create = function(data, saveCallback) {
-	var me = this;
+	me.list = function(config, callback){
+		//TODO handle paging
+		/*paramHandler.handleDefaultParams(config.something, function(params){
+			if (params !== null){
+				models.rawFeed.find().limit(params.count).skip(params.offset).sort({_id: params.sort}).execFind(callback);
+			} else {
+				models.rawFeed.find({}, callback);
+			}
+		});*/
+		
+		models.rawFeed.find({}, callback);
+	};
 	
-	me.validateFeed(data, function(valid) {
-		if (valid.valid) {
-			me.logger.info("Valid raw_feed");
-			var newFeed = new me.models.rawFeed(data);
-			newFeed.createdDate = new Date();
-			newFeed.updatedDate = new Date();
-			newFeed.save(function(err){
-				if(err){
-					me.logger.error('Error saving location', err);
-				} else {
-					actionEmitter.saveFeedEvent({data: newFeed});
-				}
-
-				saveCallback(err, valid, newFeed);
-			});
-		}
-		else {
-			saveCallback(undefined, valid, data);
-		}
-	});
-};
-
-RawFeed.prototype.validateFeed = function(data, valCallback) {
-	var me = this;
-	// is the JSON semantically valid for the location object?
-	var valid = revalidator.validate(data, me.models.rawFeedValidation);
-	if (valid.valid) {
-		// does the location object comply with business validation logic
-		//bvalidator.validate(data, function(valid) {
-		//	valCallback(valid);
-		//});
-		valCallback(valid);
-	}
-	else {
-		valCallback(valid);
-	}	
-};
-
-RawFeed.prototype.readFeedByProperty = function(property, value, readCallback){
-	var me = this;
+	/*me.count = function(config, callback){
+		models.rawFeed.find({}).count().execFind(callback);
+	};*/
 	
-	if ( (property !== undefined) && (value !== undefined) ) {
-		var query = me.models.rawFeed.find({});
-		query.where(property, value);
-		query.exec(readCallback);
-	}
-};
-
-RawFeed.prototype.update = function(id, data, updCallback) {
-	var me = this;
+	me.listFields = function(config, fields, callback){
+		models.rawFeed.find({}, fields, callback);
+	};
 	
-	this.validateFeed(data, function(valid){
-		if (valid.valid) {
-			me.models.rawFeed.findById(id, function(err, docs){
-				if(err) {
-					logger.info("Error getting raw_feed "+err);
-					updCallback(err, valid, data);
-				} else if(docs) {
-					for(var e in data){
-						//Make sure not to change _id
-						if(e !== '_id'){
-							docs[e] = data[e];
-						}
+	me.get = function(id, callback){
+		models.rawFeed.find({_id: id}, callback);
+	};
+	
+	me.getFields = function(id, fields, callback){
+		models.rawFeed.find({_id: id}, fields, callback);
+	};
+	
+	me.findWhere = function(config, callback){
+		models.rawFeed.find(config, callback);
+	};
+	
+	me.create = function(data, callback){
+		validateRawFeed(data, function(valid) {
+			if ( valid.valid ) {
+				logger.info("Valid raw feed");
+				var newFeed = new models.rawFeed(data);
+				newFeed.createdDate = new Date();
+				newFeed.updatedDate = new Date();
+				newFeed.save(function(err){
+					if (err){
+						logger.error('Error saving raw feed', err);
+					} else {
+						actionEmitter.saveFeedEvent({data: newFeed});
 					}
-					docs.updatedDate = new Date();
-					docs.save(function(err){
-						if(err){
-							updCallback(err, valid, data);
-						} else {
-							updCallback(err, valid, docs);
+					callback(err, valid, newFeed);
+				});
+			} else {
+				callback(undefined, valid, data);
+			}
+		});
+	};
+	
+	me.update = function(id, data, callback) {
+		validateRawFeed(data, function(valid){
+			if (valid.valid) {
+				me.findWhere({_id: id}, function(err, docs){
+					if (err) {
+						logger.info("Error getting raw feed " + err);
+						callback(err, valid, data);
+					} else if (docs) {
+						for (var e in data){
+							//Make sure not to change _id
+							if (e !== '_id'){
+								docs[e] = data[e];
+							}
 						}
-					});			
-				} else {
-					valid.valid = false;
-					valid.errors = {expected: id, message: "Raw_feed not found"};
-					updCallback(err, valid, data);
-				}
-			});
+						
+						docs.updatedDate = new Date();
+						docs.save(function(err){
+							if(err){
+								callback(err, valid, data);
+							} else {
+								callback(err, valid, docs);
+							}
+						});			
+					} else {
+						valid.valid = false;
+						valid.errors = {expected: id, message: "Raw feed not found"};
+						callback(err, valid, data);
+					}
+				});
+			}
+			else {
+				callback(undefined, valid, data);
+			}
+		});
+	};
+	
+	me.del = function(config, deleteCallback){
+		models.rawFeed.remove(config, deleteCallback);
+	};
+	
+	var validateRawFeed = function(data, valCallback) {
+		var services = {rawFeed: me};
+		
+		// is the JSON semantically valid for the location object?
+		var valid = revalidator.validate(data, models.rawFeedValidation);
+		if (valid.valid) {
+			// does the location object comply with business validation logic
+			//bvalidator.validate(data, function(valid) {
+			//	valCallback(valid);
+			//});
+			valCallback(valid);
 		}
 		else {
-			updCallback(undefined, valid, data);
-		}
-	});
-};
-
-RawFeed.prototype.del = function(params, deleteCallback){
-	var me = this;
-	
-	me.models.rawFeed.remove(params, deleteCallback);
+			valCallback(valid);
+		}	
+	};
 };
