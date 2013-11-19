0a5aa44022961073664ff44b6ede1b6a26c048af
Ashley <aviendha2281@gmail.com>
2013-11-19 17:18:32 -0500

    adding group and class fields so that target event draw widget is able to load target events properly

--- models/target_assertion/model.js
+++ models/target_assertion/model.js
@@ -6,25 +6,28 @@ var entity = new Schema({
 	value: {type: String, required: true},
 	x: {type: Number},
 	y: {type: Number},
-	color: {type: String, trim:true}
+	color: {type: String, trim:true},
+	group: {type: Number},
+	class: {type: String}
 });
 
 var relationship = new Schema({
 	name: {type: String},
 	value: {type: String, required: true},
-	color: {type: String, trim:true}
+	color: {type: String, trim:true},
+	class: {type: String}
 });
 
 var targetAssertionModel = {
-		createdBy: {type: Schema.Types.ObjectId},		// value supplied by the service of the profile creating the object
-		updatedBy: {type: Schema.Types.ObjectId},		// value supplied by the service of the profile updating the object
-		createdDate: {type: Date, "default": Date.now},	// value supplied by the service
-		updatedDate: {type: Date, "default": Date.now},	// value supplied by the service
-		name: {type: String, required: true},			// text identifier of the assertion
-		description: {type: String},					// description of the assertion
-		entity1: {type: [entity], required: true},
-		relationship: {type: [relationship]},
-		entity2: {type: [entity]}
+	createdBy: {type: Schema.Types.ObjectId},		// value supplied by the service of the profile creating the object
+	updatedBy: {type: Schema.Types.ObjectId},		// value supplied by the service of the profile updating the object
+	createdDate: {type: Date, "default": Date.now},	// value supplied by the service
+	updatedDate: {type: Date, "default": Date.now},	// value supplied by the service
+	name: {type: String, required: true},			// text identifier of the assertion
+	description: {type: String},					// description of the assertion
+	entity1: {type: [entity], required: true},
+	relationship: {type: [relationship]},
+	entity2: {type: [entity]}
 };
 
 var TargetAssertionSchema = new Schema(targetAssertionModel);
