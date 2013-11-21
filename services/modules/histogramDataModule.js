618e9806959f9a5052dcaef15f6cd80799beb1ab
Micah Fulton <micah.fulton@nextcentury.com>
2013-11-20 19:57:26 -0500

    Updates to histogram backend

--- services/modules/histogramDataModule.js
+++ services/modules/histogramDataModule.js
@@ -1,21 +1,19 @@
 var async = require('async');
 
 module.exports = function(model) {
-	this.findDatesByFrequency = function(mode, basedate, callback){
+	this.findDatesByFrequency = function(mode, basedate, query, callback){
 	//Get the base year, month, day, hour, minute for comparision.	
 	var tempBasedate = new Date(parseInt(basedate)).getFullYear();
 	var tempBasedateMonth= new Date(parseInt(basedate)).getMonth();
 	var tempBasedateDay= new Date(parseInt(basedate)).getDate();
 	var tempBasedateHour= new Date(parseInt(basedate)).getUTCHours();
 	var tempBasedateMinute= new Date(parseInt(basedate)).getUTCMinutes();
-
 	model.find({},{_id: 0, createdDate: 1}, function(err, dates) {
 		var errorMsg = new Error("Could not get feed Dates: " + err);
 		if(err) {
 			callback(errorMsg);
 		} else {
 			async.map(dates, flattenArray, function(err,results) {
-				
 				if(err) {
 					callback(errorMsg);
 				} else {
@@ -64,25 +62,37 @@ module.exports = function(model) {
 			});
 		}
 	});
+	
+	var getBaseDates = function(date) {
+		return {'baseYear': new Date(parseInt(date)).getUTCFullYear(), 'baseMonth': new Date(parseInt(date)).getUTCMonth(),
+		'baseDay': new Date(parseInt(date)).getUTCDate(), 'baseHour': new Date(parseInt(date)).getUTCHours(),
+		'baseMinute': new Date(parseInt(date)).getUTCMinutes(), 'baseSecond': new Date(parseInt(date)).getUTCSeconds()};
+	};
+
 
 	var pushToOutputArrayCallback = function(object) {
     	var outputArray = [];
+    	var baseDateObject = getBaseDates(basedate);
 		for(var i in object) {
-			outputArray.push({date: parseInt(i), frequency: object[i]});
+			var tempObject = {date: parseInt(i), frequency: object[i]};
+			for(i in baseDateObject) {
+				tempObject[i] = baseDateObject[i];
+			}
+			outputArray.push(tempObject);
 		}
 		callback(outputArray);
-	};
-};
-	var flattenArray = function (string, callback) {
-		callback(null, Date.parse(string.createdDate));
 	};
 
 	var countDatesGivenRange = function(acc, curr) {
 		if(typeof acc[curr] == 'undefined') {
-			acc[curr] = 1;
+			acc[curr] = 1; 
 		} else {
 			acc[curr] += 1;
 		}
 		return acc;
+	};
+};
+	var flattenArray = function (string, callback) {
+		callback(null, Date.parse(string.createdDate));
 	};
 };
