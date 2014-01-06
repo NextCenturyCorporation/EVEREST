30327ab6aa02ab76aa2324ffc8208b5de41fd900
Stephanie Schneider <stephanie.schneider@nextcentury.com>
2014-01-06 13:09:19 -0500

    changes to houston factory

--- services/houston_factory.js
+++ services/houston_factory.js
@@ -12,16 +12,19 @@ HoustonFactory = {
 
 	var Houston;
 
-
+	// Creates a Houston instance and populates it with modules and rule sets
 	createHouston: function(){
 
 		Houston = require('./houston.js');
 		console.log('Creating modules');
-		this.createModules('./');
-
+		this.createModules('./database/');
+		console.log('Creating rule sets');
+		this.createRulesets('.');
 
 	};
 
+	// Create modules for Houston
+	// @param {String} moduleDir the directory to look for modules
 	createModules: function(moduleDir){
 		var fs = require('fs');
 		//Comment this out if you want to just manually require each service
@@ -29,19 +32,57 @@ HoustonFactory = {
 
 		fs.readdirSync(moduleDir).forEach(function(file) {
 			var filename = file.stripFileDesignation();
-			var module = require('./database/' + file);
-
-			console.log("creating module " + filename + ".");
-			// TO DO: Module object?
+			try {
+				var module = require(file);
+			} catch (e){
+				console.log("error creating module " + filename);
+				return;
+			}
 			Houston.registerModule(filename, module);
-
 		});
 	};
 
-	// createRulesets = function(rulesetDir){
+	// Create the rulesets from the given directory location
+	// @param {String} rulesetDir the directory to look for rulesets
+	createRulesets = function(rulesetDir){
 
+	 	var HoustonRuleSetParser = require('./houston_ruleset_parser.js');
+	 	var fs = require('fs');
+	 	var path = require('path');
+	 	fs.readdirSync(rulesetDir).forEach(function(file){
+	 		
+	 		// get file extension
+	 		var ext = path.extname(file).split('.');
+	 		ext = ext[ext.length - 1];
+	 		
+	 		// get contents of file
+	 		var contents = fs.readFileSync(file);
+	 		var json;
+ 			if(ext === "hrs"){
+ 				try {
+ 					json = Houston.HoustonRuleSetParser.parser(contents);
+ 				} catch (e){
+ 					console.log("error creating rulesets from hrs file " + file + ": " + e);
+ 					return;
+ 				}
+ 			} else if(ext === "json"){
+ 				try {
+ 					json = JSON.parse(contents);
+					} catch (e){
+						console.log("error creating rulesets from hrs file " + file + ": " + e);
+						return;
+ 				}
+ 			} else {
+ 				return;
+ 			}
+ 			var jsonArray = [].concat(json);
+	 		for (var i = jsonArray.length - 1; i >= 0; i--) {
+	 			Houston.addRulset(jsonArray[i]);
+	 		};
+	 	});
 
-
-	// };
+	};
 
 }
+
+module.exports = HoustonFactory;
